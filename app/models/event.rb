3418f7980fd0a05c71d20220e1e2fc0b96b9a788
ammarkandel <merom8703@gmail.com>
2021-02-27 17:03:48 +0200

    Add Validation To Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,8 @@ class Event < ApplicationRecord
   has_many :enrollments, foreign_key: :attended_event_id
   has_many :attendees, through: :enrollments
 
+  validates :description, :title, :location, presence: true, length: { minimum: 3, maximum: 50 }
+
   scope :past, -> { where('date_time < ?', Date.today).order(date_time: :desc) }
   scope :future, -> { where('date_time > ?', Date.today).order(:date_time) }
 end
