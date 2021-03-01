dbf872fe9d2db1062ac29a16bb2b00ff26009cbe
ammarkandel <merom8703@gmail.com>
2021-03-01 04:50:31 +0200

    Check Ruby Version

--- spec/models/enrollment_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-describe Enrollment, type: :model do
-  describe 'associations' do
-    it { should belong_to(:attendee).class_name('User') }
-    it { should belong_to(:attended_event).class_name('Event') }
-  end
-end
