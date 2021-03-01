5b5e5a112364e0a4c925cfc48d47bada81231599
ammarkandel <merom8703@gmail.com>
2021-03-01 05:29:43 +0200

    Change rails-helpers

--- spec/factories/events.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :event do
-    title { 'Title' }
-    description { 'Text' }
-    date_time { Time.now }
-    location { 'Text' }
-  end
-end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  describe 'associations' do
+    it { should have_many(:created_events).class_name('Event') }
+    it { should have_many(:enrollments) }
+    it { should have_many(:attended_events) }
+  end
+end
